e3238969793e0788037e1c4ff6c401040a00a659
Duda-Braga <mecb@EU3146020016064.local>
2025-09-19 08:57:43 -0300

    testando com navegador Firefox

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 @pytest.fixture #notacao do pytest
 def driver():
     # Setup: initialize the WebDriver
-    driver_instance = webdriver.Chrome()
+    driver_instance = webdriver.Firefox()
     yield driver_instance #executa ate aq faz uma pausa e retrona pra qm chamou ele, conceitou do pyytest
     #alem de retornar ele divide a feature em duas 
     # Teardown: close the WebDriver
