98cd58ca948550ce5bacd16959a019c68af99644
Duda-Braga <mecb@EU3146020016064.local>
2025-09-19 10:35:53 -0300

    radio button

--- Day2/test_elements.py
+++ Day2/test_elements.py
@@ -16,3 +16,16 @@ def test_check_box(driver):
     notes_input = driver.find_element(By.ID, "tree-node-notes")
     assert notes_input.is_selected()
     #time.sleep(2) - bota o time.sleep pra ve as acoes
+
+def test_check_box_commands(driver):
+    driver.get("https://demoqa.com/checkbox")
+    expand_all_button = driver.find_element(By.CSS_SELECTOR, "button[title='Expand all']")
+    expand_all_button.click()
+    # Select the checkbox 
+    notes_checkbox = driver.find_element(By.XPATH, "//label[@for='tree-node-commands']")
+    notes_checkbox.click()
+    time.sleep(2)
+    # Validate if checkbox was ticked
+    notes_input = driver.find_element(By.ID, "tree-node-commands")
+    assert notes_input.is_selected()
+    time.sleep(2) #- bota o time.sleep pra ve as acoes
